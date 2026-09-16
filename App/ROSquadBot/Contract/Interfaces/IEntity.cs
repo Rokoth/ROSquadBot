3c4cdca60b18281c5060941143829eac30ab00d1
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2026-09-16 19:01:05 +0500

    Рефактоинг

--- App/ROSquadBot/Contract/Interfaces/IEntity.cs
+++ App/ROSquadBot/Contract/Interfaces/IEntity.cs
@@ -1,9 +1,13 @@
-﻿using System;
-
-namespace ROTGBot.Contract.Interfaces
+﻿namespace ROSquadBot.Contract.Interfaces
 {
+    /// <summary>
+    /// Общий интерфейс моделей  данных
+    /// </summary>
     public interface IEntity
     {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
         Guid Id { get; set; }
     }
 }
